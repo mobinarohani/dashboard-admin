0823f96d3446c420525f17ad9adad1a5cbe61d93
mobinarohani <mobinarohani4@gmail.colm>
2023-10-23 21:26:23 +0330

    Edit areaCharts

--- src/Components/AreaCharts/AreaCharts.jsx
+++ src/Components/AreaCharts/AreaCharts.jsx
@@ -1,8 +1,86 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import './AreaCharts.css'
 import { AreaChart, Area, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer } from "recharts";
 
 export default function AreaCharts() {
+
+    let SaleData = [];
+    const [Datas, setDatas] = useState([]);
+    const [laptopLenovo, setlaptopLenovo] = useState([]);
+    const [laptopAsus, setlaptopAsus] = useState([]);
+    const [laptopHp, setlaptopHp] = useState([]);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/laptopLenovo")
+            .then((res) => {
+                return res.json();
+            })
+            .then((data) => setlaptopLenovo(Object.entries(data)))
+    }, []);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/laptopAsus")
+            .then((res) => {
+                return res.json();
+            })
+            .then((data) => setlaptopAsus(Object.entries(data)))
+    }, []);
+
+    useEffect(() => {
+        fetch("http://localhost:3000/laptopHp")
+            .then((res) => {
+                return res.json();
+            })
+            .then((data) => setlaptopHp(Object.entries(data)))
+    }, []);
+
+    useEffect(() => {
+        laptopLenovo.find(datavalue => {
+            if (datavalue[1].name == 'Jan') SaleData.push(datavalue[1].Sale);
+        })
+
+        laptopAsus.find(datavalue => {
+            if (datavalue[1].name == 'Jan') SaleData.push(datavalue[1].Sale);
+        })
+
+        laptopHp.find(datavalue => {
+            if (datavalue[1].name == 'Jan') SaleData.push(datavalue[1].Sale);
+        })
+
+        if (SaleData) {
+            let datam = {
+                name: "Jan",
+                sale1: SaleData[0],
+                sale2: SaleData[1],
+                sale3: SaleData[2]
+            }
+
+
+            setDatas(prev => {
+                return [...prev, datam]
+            })
+        }
+
+    }, []);
+
+    //     let datam={
+    //         name: "Jan",
+    //         sale1: SaleData[0],
+    //         sale2: SaleData[1],
+    //         sale3: SaleData[2]
+    //     }
+
+    // setDatas(prev=>{
+    //     return[...prev,datam]
+    // })
+
+    // setDatas(datam)
+
+
+    console.log(Datas);
+
+    // console.log( laptopLenovo[0][1].name);
+    // console.log(SaleData);
     const data = [
         {
             name: "Page A",
